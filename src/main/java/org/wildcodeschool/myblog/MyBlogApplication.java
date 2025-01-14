87610ff073c0d6fb715439ad5291bef8932f249d
Antoine Peral <peral.antoine@gmail.com>
2025-01-14 14:23:24 +0100

    [Spring 03]  Update, add .env

--- src/main/java/org/wildcodeschool/myblog/MyBlogApplication.java
+++ src/main/java/org/wildcodeschool/myblog/MyBlogApplication.java
@@ -1,5 +1,6 @@
 package org.wildcodeschool.myblog;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyBlogApplication {
 
 	public static void main(String[] args) {
+		// Charger les variables depuis le fichier .env
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// Passer les variables comme propriétés système
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(MyBlogApplication.class, args);
+
 	}
 
 }
