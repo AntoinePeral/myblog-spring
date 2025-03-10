19dcaff18f4f7a9da4bd4bdee0beaca2b9e512d4
Antoine Peral <peral.antoine@gmail.com>
2025-03-10 12:07:49 +0100

    [Spring 11] Contraintes validateurs - Mettre dans l'ordre les getters et setters articleDTO

--- src/main/java/org/wildcodeschool/myblog/dto/ArticleDTO.java
+++ src/main/java/org/wildcodeschool/myblog/dto/ArticleDTO.java
@@ -9,15 +9,6 @@ public class ArticleDTO {
     private String content;
     private LocalDateTime updatedAt;
     private LocalDateTime createdAt;
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
     private String categoryName;
     private List<String> imageUrls;
     private List<ArticleAuthorDTO> articleAuthorDTOs;
@@ -55,6 +46,14 @@ public class ArticleDTO {
 
     public void setUpdatedAt(LocalDateTime updatedAt) {
         this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 
     public String getCategoryName() {
