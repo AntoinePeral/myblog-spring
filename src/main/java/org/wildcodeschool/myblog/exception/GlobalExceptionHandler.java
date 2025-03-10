e848d6e64fc419c5dcf4b1be73c66b6d8d2a92cc
Antoine Peral <peral.antoine@gmail.com>
2025-03-10 16:05:20 +0100

    [Spring 13] Mise en place Auth login avec JWT token, UserLoginDTO, CustomUserDetailsService, JwtAuthenticationFilter, JwtService, AuthenticationService et update de l'entité User

--- src/main/java/org/wildcodeschool/myblog/exception/GlobalExceptionHandler.java
+++ src/main/java/org/wildcodeschool/myblog/exception/GlobalExceptionHandler.java
@@ -6,12 +6,19 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException exception){
@@ -37,6 +44,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGlobalException(Exception ex) {
+        log.error("Erreur interne du serveur : {}", ex.getMessage(), ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Une erreur est survenue");
     }
 
